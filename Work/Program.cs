a4fc69b7a4c63a0f7c9f1f023e92ecdec96f27c1
Aleksandr Zhegalo <zhegalo00@list.ru>
2023-03-27 21:59:05 +0300

    2

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Вывод массива в консоль
+void Print1DArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+    Console.WriteLine($"{array[array.Length - 1]}]");
+}
+
+string [] array ={"Vlad","Alex","Tom","Io"};
+Print1DArray(array);
