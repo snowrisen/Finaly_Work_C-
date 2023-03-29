613348f47cb855e4b939c79e004d7994763cda63
Aleksandr Zhegalo <zhegalo00@list.ru>
2023-03-29 20:44:46 +0300

    3

--- Work/Program.cs
+++ Work/Program.cs
@@ -9,5 +9,18 @@ void Print1DArray(string[] array)
     Console.WriteLine($"{array[array.Length - 1]}]");
 }
 
-string [] array ={"Vlad","Alex","Tom","Io"};
+string[] array = { "Vlad", "Alex", "Tom", "Io", "Petkevich" };
 Print1DArray(array);
+
+string[] GenSearchArray(string[] array, int count)
+{
+    string[] res = new string[0];
+    for (int i = 0; i <= array.Length; i++)
+    {
+        if (array[i].Length <= count)
+        {
+            res = res.Concat(new string[] { array[i] } ).ToArray();
+        }
+    }
+    return res;
+}
